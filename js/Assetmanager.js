490258e4bd54f25b485ae9ecd5c0e4b0419caa9b
amirbadbakht312-cpu <amirbadbakht312@gmail.com>
2026-07-13 03:20:34 +0330

    Create Assetmanager.js

---
+++ js/Assetmanager.js
@@ -0,0 +1,39 @@
+class AssetManager {
+    constructor() {
+        this.images = {};
+        this.loadedCount = 0;
+        this.totalCount = 0;
+        this.onAllLoaded = null;
+    }
+
+    loadImage(key, src) {
+        this.totalCount++;
+        const img = new Image();
+        img.src = src;
+        
+        img.onload = () => {
+            this.loadedCount++;
+            if (this.loadedCount === this.totalCount && this.onAllLoaded) {
+                this.onAllLoaded();
+            }
+        };
+        
+        img.onerror = () => {
+            this.loadedCount++;
+            if (this.loadedCount === this.totalCount && this.onAllLoaded) {
+                this.onAllLoaded();
+            }
+        };
+        
+        this.images[key] = img;
+        return img;
+    }
+
+    get(key) {
+        return this.images[key] || null;
+    }
+
+    isLoaded(key) {
+        return this.images[key] && this.images[key].complete && this.images[key].naturalWidth > 0;
+    }
+}
